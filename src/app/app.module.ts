2b7286ee03660f7c56ad56296bd2276a4ce2b9b7
edw0014 <edw0014@mix.wvu.edu>
2022-01-25 14:52:01 -0500

    Created header, and began working on sidebar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from 'src/app/header/header.component';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { FormControlDirective } from "@angular/forms";
+
+
+@Component({
+    selector: 'sc-sidebar',
+    templateUrl: 'sidebar.component.html',
+    styleUrls: ['sidebar.component.css']
+})
+
+export class SideBarComponent {
+    
+}
