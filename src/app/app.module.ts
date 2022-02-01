74aa804bc7c3fab3266aa83682e6f642c0edde65
edw0014 <edw0014@mix.wvu.edu>
2022-02-01 18:20:19 -0500

    Added card component and carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from 'src/app/header/header.component';
 
 import { AppComponent } from './app.component';
+import { HorizontalCard } from './card/horizontal.card.component';
+import { Carousel } from './carousel/carousel.component';
 import { SideBarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideBarComponent
+    SideBarComponent,
+    HorizontalCard,
+    Carousel
   ],
   imports: [
     BrowserModule
---
+++ src/app/card/horizontal.card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'sc-horizontal-card',
+    templateUrl: 'horizontal.card.component.html',
+    styleUrls: ['horizontal.card.component.css']
+})
+
+export class HorizontalCard {
+    
+}
